145a0eb61eb83f253cf9ecbb9870af55a22bec94
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-07-08 18:43:19 +0300

    [FT] Change deliver commands

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env sh
 
-cd ../../backend
-npm install
-npm start
-echo 'Server listening on port 3100'
-
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm run build
+npm run start
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
@@ -29,6 +24,4 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+echo 'Server is running on port 3100 to see your Node.js/React application in action.'
